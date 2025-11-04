110d1eb4b64cfba480834e6e17b3410b27e977fb
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-11-04 23:05:13 +0000

    Parte do lab05

---
+++ LAB05_JuliaMizuguchiJulianaLeite/Product_JMJL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB05_JuliaMizuguchiJulianaLeite
+{
+    public class Product_JMJL
+    {
+        public int ProductId_JMJL { get; set; }
+        public string ProductName_JMJL { get; set; }
+        public string Category_JMJL { get; set; }
+        public decimal UnitPrice_JMJL { get; set; }
+        public int UnitsInStock_JMJL { get; set; }
+
+        override
+            public string ToString()
+        {
+            return "ID: " + ProductId_JMJL +
+                   ", Name: " + ProductName_JMJL +
+                   ", Category: " + Category_JMJL +
+                   ", Price: " + UnitPrice_JMJL +
+                   ", Stock: " + UnitsInStock_JMJL;
+        }
+    }
+}
