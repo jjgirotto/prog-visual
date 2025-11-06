7f469c97362aeafa892cef9a3b25a64f4e7edc45
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-11-06 19:56:31 +0000

    lab 02 e 03 adicionado

---
+++ Lab02JGL/Bilhete_JL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02JGL
+{
+    internal class Bilhete_JL
+    {
+        public const decimal PRECO_BILHETE_JL = 2.50M;
+        // indentificadores em c#
+        // public (ou internal) -> PascalCase
+        // tudo o resto -> cameCase
+
+        // MANTRA 1
+        // BPP - campos (atributos do java) -> SEMPRE PRIVATE
+        static int bilhetesEmitidos_JL;
+        int numeroDoBilhete_JL;
+        DateTime dataHora_JL;
+        string nomeDoCliente_JL;
+
+        // MANTRA 2 Só codificamos UM construtor
+        // Porque evitamos a todo o custo a duplicação de código
+        public Bilhete_JL(string nomeDoCliente_JL)
+        {
+            this.nomeDoCliente_JL = nomeDoCliente_JL;
+            dataHora_JL = DateTime.Now;
+            bilhetesEmitidos_JL++;
+            numeroDoBilhete_JL = bilhetesEmitidos_JL;
+        }
+
+        public Bilhete_JL() : this("Sem nome")
+        {
+            
+        }
+        public DateTime DataHora_JL
+        {
+            get { return dataHora_JL; }
+        }
+
+        public int NumeroDoBilhete_JL
+        {
+            get { return numeroDoBilhete_JL; }
+        }
+
+        public string NomeDoCliente_JL
+        {
+            get { return nomeDoCliente_JL; }
+            set { nomeDoCliente_JL = value; }
+        }
+
+        override public String ToString()
+        {
+            return
+            $@"********************************************
+            ** Bilhete Número: {numeroDoBilhete_JL} *** {dataHora_JL:yyyy/M/d HH:mm:ss}
+            ** {nomeDoCliente_JL}
+            ********************************************";
+        }
+
+
+    }
+}
---
+++ Lab02JGL/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Lab02JGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nXXXXXXX NIVEL 1 – Classe Bilhete!");
+
+            Bilhete_JL bilhete1_JL = new Bilhete_JL();
+            Console.WriteLine($"Preço do Bilhete: {Bilhete_JL.PRECO_BILHETE_JL}");
+            Console.WriteLine($"Número do Bilhete: {bilhete1_JL.NumeroDoBilhete_JL}");
+            Console.WriteLine($"Data e Hora: {bilhete1_JL.DataHora_JL}");
+            Console.WriteLine($"Nome do Cliente: {bilhete1_JL.NomeDoCliente_JL}");
+            bilhete1_JL.NomeDoCliente_JL = "Juliana";
+
+            Console.WriteLine($"Número do Bilhete: {bilhete1_JL.NumeroDoBilhete_JL}");
+            Console.WriteLine($"Data e Hora: {bilhete1_JL.DataHora_JL}");
+            Console.WriteLine($"Nome do Cliente: {bilhete1_JL.NomeDoCliente_JL}");
+
+            Console.WriteLine("\nXXXXXXX NIVEL 2 – Construtores da classe Bilhete");
+            Bilhete_JL bilhete2_JL = new Bilhete_JL("Juliana Leite");
+            Console.WriteLine($"Número do Bilhete: {bilhete2_JL.NumeroDoBilhete_JL}");
+            Console.WriteLine($"Data e Hora: {bilhete2_JL.DataHora_JL}");
+            Console.WriteLine($"Nome do Cliente: {bilhete2_JL.NomeDoCliente_JL}");
+
+            Console.WriteLine("\nXXXXXXX NIVEL 3 – Método ToString da classe Bilhete_JL");
+            Bilhete_JL bilhete3_JL = new Bilhete_JL("Ana Clara");
+            Console.WriteLine(bilhete3_JL.ToString());
+
+            Console.WriteLine("\nXXXXXXX NIVEL 4 – Venda de Bilhetes");
+            Console.Write("Insira a quantia desejada: ");
+            decimal quantia = Convert.ToDecimal(Console.ReadLine());
+            if (quantia < Bilhete_JL.PRECO_BILHETE_JL)
+            {
+                Console.WriteLine("Valor inserido é menor que o preço do bilhete.");
+            }
+            else
+            {
+                Console.Write("Informe o nome: ");
+                string nome = Console.ReadLine();
+                Bilhete_JL bilhete4_JL = new Bilhete_JL(nome);
+                decimal troco = quantia - Bilhete_JL.PRECO_BILHETE_JL;
+                Console.WriteLine($"\nBilhete comprado! Troco = {troco}");
+                Console.WriteLine(bilhete4_JL.ToString());
+            }
+
+            Console.WriteLine("\nXXXXXXX NIVEL 5 – ifs encadeados");
+            Console.Write("Insira a quantia desejada: ");
+            decimal valor = Convert.ToDecimal(Console.ReadLine());
+            if (valor < Bilhete_JL.PRECO_BILHETE_JL)
+            {
+                Console.WriteLine("Valor inserido é menor que o preço do bilhete.");
+            } else if (valor > Bilhete_JL.PRECO_BILHETE_JL)
+            {
+                Console.WriteLine("Valor inserido é maior que o preço do bilhete.");
+            }
+            else
+            {
+                Console.Write("Informe o nome: ");
+                string nome = Console.ReadLine();
+                Bilhete_JL bilhete5_JL = new Bilhete_JL(nome);
+                Console.WriteLine($"\nBilhete comprado!");
+                Console.WriteLine(bilhete5_JL.ToString());
+            }
+
+            Console.WriteLine("\nXXXXXXX NIVEL 6 – Estruturas de repetição");
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.Write("Insira a quantia desejada: ");
+                decimal valor2 = Convert.ToDecimal(Console.ReadLine());
+
+                if (valor2 < Bilhete_JL.PRECO_BILHETE_JL)
+                {
+                    Console.WriteLine("Valor inserido é menor que o preço do bilhete.");
+                }
+                else
+                {
+                    Console.Write("Informe o nome: ");
+                    string nome = Console.ReadLine();
+
+                    Bilhete_JL bilhete6_JL = new Bilhete_JL(nome);
+                    decimal troco = valor2 - Bilhete_JL.PRECO_BILHETE_JL;
+
+                    Console.WriteLine($"\nBilhete comprado! Troco = {troco}");
+                    Console.WriteLine(bilhete6_JL.ToString());
+                }
+
+                Console.Write("\nDeseja comprar outro bilhete? (s/n): ");
+                string resposta = Console.ReadLine();
+
+                if (resposta.ToLower() != "s")
+                {
+                    continuar = false;
+                }
+            }
+
+            Console.WriteLine("\nObrigado por utilizar a Máquina de Bilhetes!");
+
+        }
+    }
+}
